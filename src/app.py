2de4f4bfa18d92e9b8df0a70ce5eaaebca697c65
Giorgos Kosmas <44445106+gkosm314@users.noreply.github.com>
2021-06-16 12:17:35 +0300

    Changed app.py
    Changed imports
    Added max_threads initialization in App().__init__
    Removed hash function related functions
    Added (self) parameter to the method
    Filled App.create()

--- src/app.py
+++ src/app.py
@@ -1,11 +1,13 @@
-import misc_commands
-from create import is_valid_db_path, create
+from create import is_valid_db_path, create as create_create
 from os.path import abspath
 
-class app:
+class App:
 	#Class initialization
 	#TODO = initialize max_threads
 	def __init__(self, used_database_path_param = None):
+
+		#By default our app runs sequentially
+		self.max_threads = 1
 
 		if used_database_path_param == None:
 			#If no specific database-to-use is set, set the appropriate flag to False.
@@ -23,63 +25,55 @@ class app:
 				self.used_database_path = None
 				print(e)
 			else:
+				#This will be probably become part of use
 				#If you succeeded in using a database, set the appropriate flag to True.
 				self.database_used_flag = True
 				self.used_database_path = db_absolute_path
 
-	def help(command_param = None):
+	def help(self, command_param = None):
 		#help command implementation here...
 		pass
 
-	def version():
+	def version(self):
 		#version command implementation here...
 		pass
 
-	def about():
+	def about(self):
 		#about command implementation here...
 		pass
 
-	def exit():
+	def exit(self):
 		#exit command implementation here...
 		#__del__ constructor will be called in here
 		pass
 
-	def threads(max_threads_number_parameter):
+	def threads(self,max_threads_number_parameter):
 		#threads command implementation here...
 		pass
 
-	def create(path_param):
-		#create command implementation here...
-		pass
+	def create(self, path_param, overwrite_flag = False):
+		create_create(path_param, overwrite_flag)
 
-	def use(path_param):
+	def use(self, path_param):
 		#use command implementation here...
 		pass
 
-	def unuse():
+	def unuse(self):
 		#unuse command implementation here...
 		pass
 
-	def status():
+	def status(self):
 		#status command implementation here...
 		pass
 
-	def schema():
+	def schema(self):
 		#schema command implementation here...
 		pass
 
-	def import(import_database_path_param, import_file_path_param, import_file_format_param, overwrite_flag = False):
+	def import_db(self, import_database_path_param, import_file_path_param, import_file_format_param, overwrite_flag = False):
 		#import command implementation here...
 		pass
 
-	def export(export_database_path_param, export_file_path_param, export_file_format_param, overwrite_flag = False):
+	def export_db(self, export_database_path_param, export_file_path_param, export_file_format_param, overwrite_flag = False):
 		#export command implementation here...
-		pass
-
-	def hash_functions(details_flag = False):
-		#hash_functions command implementation here...
-		pass
-
-	def hash_is_available(hash_name_param):
-		#hash_is_available command implementation here...
 		pass
