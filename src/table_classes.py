7cf96217f3e4cc2cd82eb22016ab3e8839991b8a
gkosm314 <gkosm314@gmail.com>
2021-07-16 03:06:29 +0300

    Defined relationships mapping/Made minor changes to the schema

--- src/table_classes.py
+++ src/table_classes.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, BigInteger, Integer, Boolean, String, Text, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -13,24 +14,35 @@ class DbInformation(Base):
    db_version = Column(Integer)
    db_last_scan_id = Column(Integer)
 
+
 class ScanCode(Base):
    __tablename__ = 'SCAN_CODE'
    scan_return_code = Column(Integer, primary_key = True)
-   scan_return_code_description = Column(Integer)
+   scan_return_code_description = Column(String)
+
+   scans = relationship("Scan", back_populates="scan_code")
+
 
 class Scan(Base):
    __tablename__ = 'SCAN'
    scan_id = Column(Integer, primary_key = True)
    scan_hostname = Column(String)
-   scan_command_executed = Column(Text)
    scan_date = Column(DateTime)
    scan_return_code = Column(Integer, ForeignKey('SCAN_CODE.scan_return_code'))
+
+   scan_code = relationship("ScanCode", back_populates="scans")
+   files = relationship("File", back_populates="scan")
+
 
 class Origin(Base):
    __tablename__ = 'ORIGIN'
    origin_id = Column(Integer, primary_key = True)
    origin_is_local_flag = Column(Boolean)
    origin_url_or_hostname = Column(String)
+   origin_retrival_datetime = Column(DateTime)
+
+   files = relationship("File", back_populates="origin")
+
 
 class File(Base):
    __tablename__ = 'FILE'
@@ -46,6 +58,11 @@ class File(Base):
    file_updated = Column(Boolean)
    origin_id = Column(Integer, ForeignKey('ORIGIN.origin_id'))
 
+   scan = relationship("Scan", back_populates="files")
+   origin = relationship("Origin", back_populates="files")
+   hashes = relationship("Hash", secondary="HASH_TO_FILE")
+
+
 class Hash(Base):
    __tablename__ = 'HASH'
    hash_id = Column(Integer, primary_key = True)
@@ -53,15 +70,20 @@ class Hash(Base):
    hash_function_name = Column(String, ForeignKey('HASH_FUNCTION.hash_function_name'))
    file_id = Column(Integer, ForeignKey('FILE.file_id'))
 
+   hash_function = relationship("HashFunction", back_populates="hashes")
+   files = relationship("File", secondary="HASH_TO_FILE")
+
+
 class HashFunction(Base):
    __tablename__ = 'HASH_FUNCTION'
    hash_function_name = Column(String, primary_key = True)
    hash_function_fuzzy_flag = Column(Boolean)
    hash_function_size = Column(Integer)
 
+   hashes = relationship("Hash", back_populates="hash_function")
 
-class SwhInfo(Base):
-   __tablename__ = 'SWH_INFO'
+
+class HashToFile(Base):
+   __tablename__ = 'HASH_TO_FILE'
+   hash_id = Column(Integer, ForeignKey('HASH.id'), primary_key = True)
    file_id = Column(Integer, ForeignKey('FILE.file_id'), primary_key = True)
-   swh_id_core = Column(Integer)
-   swh_id_qualifiers = Column(Integer)
