bacdbc423b8047eebcbf8e1769fec815bb7d5277
gkosm314 <gkosm314@gmail.com>
2021-08-17 05:02:52 +0300

    Updated file

--- tests/hf_tests/test_hf.py
+++ tests/hf_tests/test_hf.py
@@ -12,7 +12,7 @@ class TestHashFunctionsFunction(unittest.TestCase):
 	def setUp(self):
 		#Suppress stdout:
 		self.io_stream = io.StringIO()
-		#sys.stdout = self.io_stream 
+		sys.stdout = self.io_stream 
 
 		self.db = Db('mytest.db')
 
@@ -23,38 +23,31 @@ class TestHashFunctionsFunction(unittest.TestCase):
 		sys.stdout = sys.__stdout__
 		self.io_stream.close()
 
-	def test_hash_functions(self):
-		expected_string = "Available hash functions:\nshake_256, blake2s, sha3_384, sha512, shake_128, sha3_224, sha384, sha224, sha3_512, sha1, md5, sha3_256, sha256, blake2b, xxh32, xxh64, ssdeep, tlsh, swhid"
-		self.assertEqual(self.io_stream.getvalue(), expected_string)	
+	def test_hash_is_available_available(self):
+		self.assertTrue(self.db.hash_is_available('md5'))
 
-	def test_hash_functions_details(self):
-		pass
+	def test_hash_is_available_not_available(self):
+		self.assertFalse(self.db.hash_is_available('whatever'))	
 
-	# def test_hash_is_available_available(self):
-	# 	self.assertTrue(self.db.hash_is_available('md5'))
+	def test_is_valid_hash_functions_valid(self):
+		returned_list = self.db.valid_hash_functions(['md5','sha1','ssdeep','tlsh'])
+		returned_list.sort()
+		self.assertEqual(returned_list, ['md5', 'sha1', 'ssdeep', 'tlsh'])
 
-	# def test_hash_is_available_not_available(self):
-	# 	self.assertFalse(self.db.hash_is_available('whatever'))	
+	def test_is_valid_hash_functions_remove_duplicates(self):
+		returned_list = self.db.valid_hash_functions(['md5','sha1','sha1','md5','ssdeep','tlsh'])
+		returned_list.sort()
+		self.assertEqual(returned_list, ['md5', 'sha1', 'ssdeep', 'tlsh'])
 
-	# def test_is_valid_hash_functions_valid(self):
-	# 	returned_list = self.db.valid_hash_functions(['md5','sha1','ssdeep','tlsh'])
-	# 	returned_list.sort()
-	# 	self.assertEqual(returned_list, ['md5', 'sha1', 'ssdeep', 'tlsh'])
+	def test_is_valid_hash_functions_remove_swhid(self):
+		returned_list = self.db.valid_hash_functions(['md5','swhid','sha1','ssdeep','tlsh'])
+		returned_list.sort()
+		self.assertEqual(returned_list, ['md5', 'sha1', 'ssdeep', 'tlsh'])
 
-	# def test_is_valid_hash_functions_remove_duplicates(self):
-	# 	returned_list = self.db.valid_hash_functions(['md5','sha1','sha1','md5','ssdeep','tlsh'])
-	# 	returned_list.sort()
-	# 	self.assertEqual(returned_list, ['md5', 'sha1', 'ssdeep', 'tlsh'])
-
-	# def test_is_valid_hash_functions_remove_swhid(self):
-	# 	returned_list = self.db.valid_hash_functions(['md5','swhid','sha1','ssdeep','tlsh'])
-	# 	returned_list.sort()
-	# 	self.assertEqual(returned_list, ['md5', 'sha1', 'ssdeep', 'tlsh'])
-
-	# def test_is_valid_hash_functions_remove_invalid(self):
-	# 	returned_list = self.db.valid_hash_functions(['md5','sha1','whatever1','whatever2','ssdeep','tlsh'])
-	# 	returned_list.sort()
-	# 	self.assertEqual(returned_list, ['md5', 'sha1', 'ssdeep', 'tlsh'])							
+	def test_is_valid_hash_functions_remove_invalid(self):
+		returned_list = self.db.valid_hash_functions(['md5','sha1','whatever1','whatever2','ssdeep','tlsh'])
+		returned_list.sort()
+		self.assertEqual(returned_list, ['md5', 'sha1', 'ssdeep', 'tlsh'])							
 
 if __name__ == '__main__':
 	unittest.main()
